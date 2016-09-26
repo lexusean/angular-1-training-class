2f0d7edd330750f624c73b587ed1369dbc08ba5e
SEAN BORDEN <SBORDEN@MICRON.COM>
2016-09-26 14:07:37 -0600

    step 5 push

--- src/common/hero/heroService.js
+++ src/common/hero/heroService.js
@@ -3,90 +3,38 @@ angular
   ])
   .factory('heroService', heroService);
 
-function heroService() {
+function heroService($http, $q) {
   
-  var heroes = [
-    {
-      id: 0,
-      name: 'Luke Skywalker',
-      imageUrl: 'heros/images/luke.png',
-      power: 9000,
-      affiliations: ['Jedi', 'Rebel'],
-      light: true
-    },
-    {
-      id: 1,
-      name: 'R2D2',
-      imageUrl: 'heros/images/r2d2.png',
-      power: 2000,
-      affiliations: ['Droid', 'Rebel'],
-      light: true
-    },
-    {
-      id: 2,
-      name: 'Chewie',
-      imageUrl: 'heros/images/chewie.png',
-      power: 6000,
-      affiliations: ['Scoundrel', 'Rebel'],
-      light: true
-    },
-    {
-      id: 3,
-      name: 'Darth Maul',
-      imageUrl: 'heros/images/darthMaul.png',
-      power: 8000,
-      affiliations: ['Sith'],
-      light: false
-    },
-    {
-      id: 4,
-      name: 'Darth Vader',
-      imageUrl: 'heros/images/darthVader.png',
-      power: 10000,
-      affiliations: ['Sith', 'Empire'],
-      light: false
-    },
-    {
-      id: 5,
-      name: 'Jango Fett',
-      imageUrl: 'heros/images/jangoFett.png',
-      power: 7000,
-      affiliations: ['Bounty Hunter', 'Mandalorian'],
-      light: false
-    },
-    {
-      id: 14,
-      name: 'Captain Rex',
-      imageUrl: 'http://www.sideshowtoy.com/wp-content/uploads/2014/02/100222-product-silo3.png',
-      power: 2000,
-      affiliations: ['Clone Trooper', 'New Republic'],
-      light: true
-    },
-    {
-      id: 15,
-      name: 'Andy',
-      imageUrl: 'heros/images/aswanson.jpeg',
-      power: 40000,
-      affiliations: ['Beer', 'Crown', 'Boating', 'D-Bag'],
-      light: false
-    },
-  ];
+  var heroes = [];
 
   var service = {
     getAll: getAll,
-    get: get,
+    get: get
   };
 
   function getAll() {
     console.log('hero::heroService::getAll');
-    return heroes;
+    if (heroes.length === 0) {
+      // get heroes from data/heroes.json
+      // and set heroes to the value returned in the resonse
+      return $http.get('heros/data/heros.json').then(function(res) {
+        heroes = res.data;
+        return heroes;
+      });
+    } else {
+      // return heroes in a promise
+      return $q.when(heroes);
+    }
   }
 
   function get(id) {
-    var h = heroes.filter(function(hero) {
-      return hero.id === id;
-    });
-    return h[0];
+    return service.getAll().then(function(heroes) {
+      // move filtering here
+      var h = heroes.filter(function(hero) {
+        return hero.id === id;
+      });
+      return h[0];
+    }); 
   }
 
   return service;
