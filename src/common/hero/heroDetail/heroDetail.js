a7660bdaf88ac5607f55b2881a1de5713163d44a
SEAN BORDEN <SBORDEN@MICRON.COM>
2016-09-26 13:23:45 -0600

    Yay

---
+++ src/common/hero/heroDetail/heroDetail.js
@@ -0,0 +1,20 @@
+angular
+  .module('heroDetail', [
+  ])
+  .directive('heroDetail', heroDetail);
+
+function heroDetail() {
+    return {
+        restrict: 'E',
+        scope: {
+            hero: '=',
+            onClose: '&'
+        },
+        controller: heroDetailController,
+        templateUrl: 'heros/src/common/hero/heroDetail/heroDetail.tpl.html'
+    };
+}
+
+function heroDetailController($scope) {
+    console.log('in hero detail controller');
+}
